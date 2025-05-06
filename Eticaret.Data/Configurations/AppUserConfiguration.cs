6f5c4e1e251ba4cc1579cc84aa9d115d2fff9f32
Gökhan <gokhan.durakoglugil98@gmail.com>
2025-05-06 20:16:06 +0300

    Add project files.

---
+++ Eticaret.Data/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Eticaret.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eticaret.Data.Configurations
+{
+    internal class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(x => x.Name).IsRequired().HasColumnType("varchar(50)").HasMaxLength(50);
+            builder.Property(x => x.Surname).IsRequired().HasColumnType("varchar(50)").HasMaxLength(50);
+            builder.Property(x => x.Email).IsRequired().HasColumnType("varchar(50)").HasMaxLength(50);
+            builder.Property(x => x.Phone).HasColumnType("varchar(50)").HasMaxLength(15);
+            builder.Property(x => x.Password).HasColumnType("varchar(50)").IsRequired().HasMaxLength(50);
+            builder.Property(x => x.UserName).HasColumnType("varchar(50)").HasMaxLength(50);
+            builder.HasData(
+               new AppUser
+               {
+                   Id = 1,
+                   UserName = "Admin",
+                   Email = "admin@eticaret.io",
+                   IsActive = false,
+                   IsAdmin = true,
+                   Name = "test",
+                   Surname = "test",
+                   Password = "123456*",
+
+               }
+                );
+        }
+    }
+}
---
+++ Eticaret.Data/DatabaseContext.cs
@@ -0,0 +1,34 @@
+﻿using Eticaret.Core.Entities;
+using Eticaret.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+
+namespace Eticaret.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+               .UseSqlServer(@"Server=.;Database=EticaretDb;Trusted_Connection=True;TrustServerCertificate=True;")
+               .ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+            
+            base.OnConfiguring(optionsBuilder);
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
